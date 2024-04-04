0c93a8fdaf801e2efcf15fdd4fa979e8736a7897
Chandana S <114733441+Chandanaschandu@users.noreply.github.com>
2024-04-04 21:03:23 +0530

    Add files via upload

---
+++ src/hostel_management_system/Hostel_Management_System.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hostel_management_system;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.*;
+/**
+ *
+ * @author ANKITHA R
+ */
+public class Hostel_Management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+        
+    }
+    
+}
